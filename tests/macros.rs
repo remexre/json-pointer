efa205f325d9eda2d489d8c6e840df42092e1bd7
Nathaniel Ringo <remexre@gmail.com>
2017-06-07 12:31:32 -0500

    Test fixes.

--- tests/macros.rs
+++ tests/macros.rs
@@ -1,16 +1,10 @@
-extern crate json_pointer;
-
-use json_pointer::JsonPointer;
-
 macro_rules! assert_unparse {
     ($expr:expr) => {
         let ptr = $expr.parse::<JsonPointer<_, _>>().unwrap();
+        assert_eq!(ptr.to_string(), $expr);
+    };
+    (uri $expr:expr) => {
+        let ptr = $expr.parse::<JsonPointer<_, _>>().unwrap();
         assert_eq!(ptr.uri_fragment(), $expr);
     };
-}
-
-#[test]
-fn uri_fragment_unparses() {
-    assert_unparse!("#/");
-    assert_unparse!("#/per%25/%25cent");
 }
